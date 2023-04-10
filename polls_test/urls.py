b7c452a634abbcfb8cc08305754b80d3a121a712
äyuyazi <ayuyazi@gmail.com>
2023-04-10 10:20:20 +0300

    Ay

--- polls_test/urls.py
+++ polls_test/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('groups',views.display_group,name='display_group'),
     path('user',views.display_join, name='user'),
     path('<str:user_id>/user_reg',views.user_reg, name='user_reg')
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
