bb54b83f4062f76b1f0262b9e599771e7b65b3d1
äyuyazi <ayuyazi@gmail.com>
2023-04-10 10:47:03 +0300

    Ayuy

--- D2015/settings.py
+++ D2015/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,6 +129,13 @@ MEDIA_URL = 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "polls_test/templates/static",
+'''STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
+
+'''
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join (BASE_DIR,'staticfiles')
